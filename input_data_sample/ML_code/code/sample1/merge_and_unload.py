2cb2c78d82e5a4ecdf963a062ff73212e9b37403
Kateryna Zakharova <49046456+KaterynaZakharova@users.noreply.github.com>
2026-05-18 19:25:30 -0400

    Add final notebook, example data, requirements

---
+++ input_data_sample/ML_code/code/sample1/merge_and_unload.py
@@ -0,0 +1,32 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, pipeline
+from peft import PeftModel
+import torch
+
+from config import MERGE_ADAPTER_CONFIGS
+
+base_model_url = MERGE_ADAPTER_CONFIGS["base_model_url"]
+adapter_model_url = MERGE_ADAPTER_CONFIGS["adapter_model_url"]
+save_dir = MERGE_ADAPTER_CONFIGS["new_model_url"]
+
+
+print("[/] loading base model and tokenizer...")
+# Reload tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained(base_model_url)
+
+base_model_reload = AutoModelForCausalLM.from_pretrained(
+    base_model_url,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=MERGE_ADAPTER_CONFIGS["torch_dtype"],
+    device_map=MERGE_ADAPTER_CONFIGS["device_map"],
+    attn_implementation=MERGE_ADAPTER_CONFIGS["attn_implementation"]
+)
+
+print("[/] merging adapter weights with the base model...")
+# base_model_reload, tokenizer = setup_chat_format(base_model_reload, tokenizer)
+model = PeftModel.from_pretrained(base_model_reload, adapter_model_url)
+
+model = model.merge_and_unload()
+
+model.save_pretrained(save_dir)
+tokenizer.save_pretrained(save_dir)
